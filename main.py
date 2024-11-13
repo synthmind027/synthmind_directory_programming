0b47247a79629ab9a954b02fa8ab31a68b3742c9
synthmind027 <synthmind027@gmail.com>
2024-11-13 17:08:13 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+def root_folder():
+	d = dict()
+	d[''] = d
+	d['.'] = d
+	d['..'] = d
+	d['_name'] = 'root'
+	d['_desc'] = 'root'
+	return d
+
+def ls(d):
+	ignores = ['', '.', '..', '_name', '_desc']
+	chld = [x for x in d.keys() if x not in ignores]
+	print('\t'.join(chld))
+
+def mkdir(d, nm):
+	d[nm] = dict()
+	d[nm][''] = d['']
+	d[nm]['.'] = d[nm]
+	d[nm]['..'] = d
+	d[nm]['_name'] = nm
+	d[nm]['_desc'] = ''
+
+def folder(d, pth):
+	ret = d
+	for x in pth.split('/'):
+		if x not in ret:
+			mkdir(ret, x)
+		ret = ret[x]
+	return ret
+
+
+d = root_folder()
+folder(d, 'asdf/qwer/asdf/zxcv')
+folder(d, 'asdf/zxcv')
+ls(folder(d, 'asdf'))
